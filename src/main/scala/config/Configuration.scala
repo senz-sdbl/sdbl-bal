4cb567e3da982a5afc6d914968d9513766b711ed
eranga herath <erangaeb@gmail.com>
2016-01-11 17:00:23 +0530

    added db config

--- src/main/scala/config/Configuration.scala
+++ src/main/scala/config/Configuration.scala
@@ -18,4 +18,9 @@ trait Configuration {
   lazy val serviceHost = Try(config.getString("service.host")).getOrElse("localhost")
   lazy val servicePort = Try(config.getInt("service.port")).getOrElse(9999)
 
+  // db config
+  lazy val dbHost = Try(config.getString("db.host")).getOrElse("dev.localhost")
+  lazy val dbPort = Try(config.getInt("db.port")).getOrElse(27017)
+  lazy val dbName = Try(config.getString("db.name")).getOrElse("senz")
+
 }
