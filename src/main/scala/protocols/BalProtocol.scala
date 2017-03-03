452145afcbd4e8fc03e68fc29593d19fd1ae2833
laksith1 <0756939469>
2017-03-03 17:46:54 +0530

    updated to accept account inquery ,bal inquery,mini statement. But not complete.
    PS commited with errors

--- src/main/scala/protocols/BalProtocol.scala
+++ src/main/scala/protocols/BalProtocol.scala
@@ -2,8 +2,8 @@ package protocols
 
 case class Agent(account: String, branch: String)
 
-case class Bal(agent: String, customer: String, amount: Int, timestamp: String, status: String)
+case class BalInq(agent: String, customer: String, amount: Int, timestamp: String, status: String)
 
-case class BalMsg(msgStream: Array[Byte])
+case class BalInqMsg(msgStream: Array[Byte])
 
-case class BalResp(esh: String, status: String, rst: String)
+case class BalInqResp(esh: String, status: String, rst: String)
